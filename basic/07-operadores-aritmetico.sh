8c0ec5eb78f160a6f05c9a8094be4a2f1c9a0a07
Alfred Arista <arista21dominguez@gmail.com>
2025-05-24 23:41:57 -0600

    init II

---
+++ basic/07-operadores-aritmetico.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+num_a=32
+num_b=6
+
+echo "Suma de ${num_a} + ${num_b} = $(( ${num_a} + ${num_b} ))"
+
+echo "Resta de ${num_a} - ${num_b} = $(( ${num_a} - ${num_b} ))"
+
+echo "Multiplicacion de ${num_a} * ${num_b} = $(( ${num_a} * ${num_b} ))"
+
+echo "Divicion de ${num_a} / ${num_b} = $(( ${num_a} / ${num_b} ))"
+
+echo "Modulo de ${num_a} % ${num_b} = $(( ${num_a} % ${num_b} ))"
+
+echo "Potencia de ${num_a} ^ ${num_b} = $(( ${num_a} ** ${num_b} ))"
+
+
+
---
+++ basic/08-operadores-relacionales.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Mayor que $(( 5 > 4 ))"
+
+echo "Menor que $(( 4 < 4 ))"
+
+echo "Mayor o igual que $(( 5 >= 5 ))"
+
+echo "Menor o igual que $(( 5 <= 6))"
+
+echo "Distinto a $(( 4 != 6 ))"
+
+echo "Igual a $(( 5 == 6 ))"
